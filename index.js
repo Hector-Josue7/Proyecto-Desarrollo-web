cf01a853126cc9ce9df6a1b622d3dfcc8e314903
Hector-Josue7 <hectorardonm@gmail.com>
2018-11-24 13:49:27 -0600

    Eliminacion de archivos basura

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ var app = express();
 //Exponer una carpeta como publica, unicamente para archivos estaticos: .html, imagenes, .css, .js
 app.use(express.static("public"));
 //Crear y levantar el servidor web.
+
+app.get("/login.html",function(req,res){
+    res.send("<html><body><h1>Hola mundo</h1></body></html>");
+    res.end();
+});
+
+
 app.listen(3000);
 console.log("Servidor iniciado");
+
+
