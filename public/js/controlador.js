77e602c0f834b834edfbc14ab61b14957afab636
Hector-Josue7 <hectorardonm@gmail.com>
2018-11-24 23:17:41 -0600

    Se diseño una ventana para el plan premiun y demas beneficios

---
+++ public/js/controlador.js
@@ -0,0 +1,7 @@
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+
+if (!window.indexedDB) {
+    window.alert("Su navegador no soporta una versión estable de indexedDB. Tal y como las características no serán validas");
+}
